ff45b52d91da43f016044fcc02d5123a15794e82
Anthony Phan <90881907+aphan9817@users.noreply.github.com>
2025-01-21 17:56:46 -0500

    Add DataModel and bind data to data grid

---
+++ SENG3120-A01/SENG3120-A01/DataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENG3120_A01
+{
+    public class DataModel
+    {
+        public string Category { get; set; }
+        public int Frequency { get; set; }
+        public double Percentage { get; set; }
+    }
+}
--- SENG3120-A01/SENG3120-A01/MainWindow.xaml.cs
+++ SENG3120-A01/SENG3120-A01/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OxyPlot;
 using OxyPlot.Axes;
+using OxyPlot.Legends;
 using OxyPlot.Series;
 
 namespace SENG3120_A01
@@ -27,13 +28,40 @@ namespace SENG3120_A01
         {
             InitializeComponent();
 
-            var categories = new[] { "Test" };
-            var frequencies = new[] { 1 };
+            var categories = new[] { "Test 1", "Test 2", "Test 3", "Test 4", "Test 5", };
+            var frequencies = new[] { 1, 2, 30, 40, 50 };
+            int total = frequencies.Sum();
+
+            double cumulative = 0;
+            var dataList = new List<DataModel>();
+            var percentages = new List<double>();
+
+            for (int i = 0; i < frequencies.Length; i++)
+            {
+                cumulative += frequencies[i];
+                double Percentage = (cumulative / total) * 100;
+                percentages.Add(Percentage);
+
+                dataList.Add(new DataModel
+                {
+                    Category = categories[i],
+                    Frequency = frequencies[i],
+                    Percentage = Percentage,
+                });
+            }
+
+            DataGrid.ItemsSource = dataList;
 
             // title
             var plotModel = new PlotModel { Title = "Pareto Diagram" };
 
             // legend
+            plotModel.Legends.Add(new Legend
+            {
+                LegendTitle = "Legend",
+                LegendPosition = LegendPosition.TopRight,
+                LegendFontSize = 12
+            });
             
             // axis titles
             plotModel.Axes.Add(new CategoryAxis
@@ -62,7 +90,6 @@ namespace SENG3120_A01
             });
 
             // axis values
-
             MyModel.Model = plotModel;
         }
     }
